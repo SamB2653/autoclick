0a403a983f4b1f9976d823b5578555815ae7aa80
SamB2653 <swborrington@gmail.com>
2020-12-30 11:16:10 +0000

    Added no click option

--- autoclick.py
+++ autoclick.py
@@ -2,32 +2,48 @@ import pyautogui
 import random
 import sys
 
+
+def screen_dimensions():
+    screen_size = pyautogui.size()
+    new_height = screen_size.height - 10
+    new_width = screen_size.width - 10
+    return new_height, new_width
+
+
+def random_movement(max_h, max_w):
+    y_val = random.randint(10, max_h)
+    x_val = random.randint(10, max_w)
+    pyautogui.moveTo(x_val, y_val, 2, pyautogui.easeOutQuad)
+    print(pyautogui.position())
+
+
 pyautogui.FAILSAFE = True
 pyautogui.PAUSE = 0.5
 interval = 0.5
 session = True
 
-prompt_choice = pyautogui.confirm(text='Select which corner to click', title='Auto-clicker', buttons=['LEFT', 'RIGHT'])
-screen_size = pyautogui.size()
+prompt_choice = pyautogui.confirm(text='Select which corner to click\n(end by moving your mouse to any corner of the '
+                                       'screen)', title='Auto-clicker', buttons=['LEFT', 'NO-CLICK', 'RIGHT'])
 
 try:
     if prompt_choice == 'LEFT':
+        max_height, max_width = screen_dimensions()
         while session:
-            y = random.randint(0, screen_size.height)
-            x = random.randint(0, screen_size.width)
-            pyautogui.moveTo(x, y, 2, pyautogui.easeOutQuad)
-            print(pyautogui.position())
+            random_movement(max_height, max_width)
             pyautogui.moveTo(10, 10, 2, pyautogui.easeOutQuad)
             pyautogui.click(x=10, y=10, clicks=1, interval=interval, button='left')
 
     if prompt_choice == 'RIGHT':
+        max_height, max_width = screen_dimensions()
         while session:
-            y = random.randint(0, screen_size.height)
-            x = random.randint(0, screen_size.width)
-            pyautogui.moveTo(x, y, 2, pyautogui.easeOutQuad)
-            print(pyautogui.position())
-            pyautogui.moveTo(screen_size.width - 10, 10, 2, pyautogui.easeOutQuad)
-            pyautogui.click(x=screen_size.width - 10, y=10, clicks=1, interval=interval, button='left')
+            random_movement(max_height, max_width)
+            pyautogui.moveTo(max_width, 10, 2, pyautogui.easeOutQuad)
+            pyautogui.click(x=max_width, y=10, clicks=1, interval=interval, button='left')
+
+    if prompt_choice == 'NO-CLICK':
+        max_height, max_width = screen_dimensions()
+        while session:
+            random_movement(max_height, max_width)
 
 except pyautogui.FailSafeException as e:
     pyautogui.alert("Completed Script - Fail-safe")
@@ -36,4 +52,4 @@ except KeyboardInterrupt:
     pyautogui.alert("Completed Script - Keyboard Interrupt")
     sys.exit()
 
-sys.exit()  # pyinstaller --onefile autoclick.py
+sys.exit()
